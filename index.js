ccf61b4be4204f9ede9e07a90d942cfc629f5590
eugene94 <eugenekang94@gmail.com>
2019-03-19 15:45:17 +0900

    url에 따라 download하기

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ const express = require('express');
 const app = express();
 const ytdl = require('ytdl-core');
 
+app.use(express.static('public'));
+
 app.listen(4000, () => {
   console.log('Server Works !!! At port 4000');
 });
 
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
 app.get('/download', (req, res) => {
-  // var URL = req.query.URL;
-  const URL = 'https://www.youtube.com/watch?v=ZnmEfn4t6pM';
-  // console.log(URL);
-  res.header('Content-Disposition', 'attachment; filename="test.mp4"');
-  ytdl.getInfo(URL, (err, info) => {
-    ytdl.downloadFromInfo(info).pipe(res);
-  });
+  var URL = req.query.URL;
+  // const URL = 'https://www.youtube.com/watch?v=ZnmEfn4t6pM';
+
+  ytdl
+    .getInfo(URL)
+    .then(info => {
+      // console.log(info.title);
+      res.header('Content-Disposition', `attachment; filename="video.mp4"`);
+      return Promise.resolve(info);
+    })
+    .then(info => {
+      ytdl.downloadFromInfo(info).pipe(res);
+    });
 });
